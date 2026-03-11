dcf46e49ff233a4bb98697787a24e626bde90951
cathycota-lgtm <cathycota@gmail.com>
2026-03-11 11:42:29 -0300

    Update crear-pago.js

--- api/crear-pago.js
+++ api/crear-pago.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "Authorization": "Bearer TU_ACCESS_TOKEN"
+      "Authorization": `Bearer ${process.env.MP_ACCESS_TOKEN}`
     },
     body: JSON.stringify({
       items: items,
