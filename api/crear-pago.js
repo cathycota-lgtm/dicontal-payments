95694fc63d296309c3c176098845ae07bf852633
cathycota-lgtm <cathycota@gmail.com>
2026-04-07 13:53:27 -0400

    Update crear-pago.js

--- api/crear-pago.js
+++ api/crear-pago.js
@@ -1,12 +1,14 @@
 module.exports = async function handler(req, res) {
 
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
+// manejar preflight correctamente
+if (req.method === "OPTIONS") {
+  res.status(200).json({});
+  return;
+}
 
   try {
 
