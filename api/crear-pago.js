f0f29c06828eb3dbea76c6eeffc79b89f9684b81
cathycota-lgtm <cathycota@gmail.com>
2026-04-07 13:45:31 -0400

    Update crear-pago.js

--- api/crear-pago.js
+++ api/crear-pago.js
@@ -1,6 +1,3 @@
-import { Resend } from "resend";
-
-const resend = new Resend(process.env.RESEND_API_KEY);
 module.exports = async function handler(req, res) {
 
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -28,23 +25,6 @@ module.exports = async function handler(req, res) {
     });
 
     const data = await response.json();
-    await resend.emails.send({
-  from: "onboarding@resend.dev",
-  to: "ccorrea@dicontal.cl",
-  subject: "Nuevo pedido web",
-  html: `
-    <h2>Nuevo pedido</h2>
-    <p><strong>Nombre:</strong> ${nombre}</p>
-    <p><strong>Email:</strong> ${email}</p>
-    <p><strong>Teléfono:</strong> ${telefono}</p>
-    <p><strong>Comuna:</strong> ${comuna}</p>
-    <p><strong>Dirección:</strong> ${direccion}</p>
-    <hr>
-    <p><strong>Detalle:</strong> ${items[0]?.title}</p>
-    <p><strong>Total:</strong> $${items[0]?.unit_price}</p>
-  `
-});
-
     res.status(200).json({
   init_point: data.init_point
 });
